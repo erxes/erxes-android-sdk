d2abdc32bfdd20821d2a2b0209628d89d23ae08f
Itgel Galt <itgel.atom7@gmail.com>
2020-05-15 18:19:23 +0800

    strUtil

---
+++ erxeslibrary/src/main/java/com/newmedia/erxeslibrary/utils/EnumUtil.java
@@ -0,0 +1,7 @@
+package com.newmedia.erxeslibrary.utils;
+
+public class EnumUtil {
+    public static final String TYPETEXT = "text";
+    public static final String TYPEVCALLREQUEST = "videoCallRequest";
+    public static final String TYPEVCALL = "videoCall";
+}
