dc7665680fe79e3ea128922d7a63c15f7156403f
Itgel G <itgel.g@nmtec.co>
2020-11-24 20:50:48 +0800

    knowledgeBase is suppported webview

--- erxeslibrary/src/main/java/com/newmedia/erxeslibrary/ui/faq/FaqDetailActivity.java
+++ erxeslibrary/src/main/java/com/newmedia/erxeslibrary/ui/faq/FaqDetailActivity.java
@@ -6,12 +6,20 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Base64;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +36,8 @@ public class FaqDetailActivity extends AppCompatActivity {
     private Point size;
     private Config config;
     private ImageView backImageView, cancelImageView;
+    private View loaderView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,7 @@ public class FaqDetailActivity extends AppCompatActivity {
         container = this.findViewById(R.id.container);
         backImageView = this.findViewById(R.id.backImageView);
         cancelImageView = this.findViewById(R.id.cancelImageView);
+        loaderView = this.findViewById(R.id.loaderView);
         initIcon();
 
         size = ErxesHelper.display_configure(this,container,"#00000000");
@@ -92,7 +103,7 @@ public class FaqDetailActivity extends AppCompatActivity {
         TextView articleHeader = this.findViewById(R.id.article_header);
         TextView date = this.findViewById(R.id.article_date);
         TextView content1 = this.findViewById(R.id.article_content1);
-        TextView content2 = this.findViewById(R.id.article_content2);
+        webView = this.findViewById(R.id.article_content2);
 
         String id = getIntent().getStringExtra("id");
         String categoryId = getIntent().getStringExtra("categoryId");
@@ -114,14 +125,39 @@ public class FaqDetailActivity extends AppCompatActivity {
                     }
                 }
                 if (knowledgeBaseArticle != null) {
-                    Log.e("TAG", "load_findViewByid: " + knowledgeBaseArticle.createdDate );
                     general.setText(knowledgeBaseArticle.title);
                     date.setText("Created : " + config.FullDate(knowledgeBaseArticle.createdDate));
                     articleHeader.setText(knowledgeBaseArticle.title);
                     content1.setText(Html.fromHtml(knowledgeBaseArticle.summary));
                     content1.setMovementMethod(LinkMovementMethod.getInstance());
-                    content2.setText(Html.fromHtml(knowledgeBaseArticle.content));
-                    content2.setMovementMethod(LinkMovementMethod.getInstance());
+                    webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+                    webView.getSettings().setLoadWithOverviewMode(true);
+                    webView.getSettings().setUseWideViewPort(true);
+
+                    webView.getSettings().setBuiltInZoomControls(true);
+                    webView.getSettings().setDomStorageEnabled(true);
+                    webView.getSettings().setLoadsImagesAutomatically(true);
+                    webView.clearCache(false);
+                    webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+                    webView.setWebViewClient(new WebViewClient() {
+                        @Override
+                        public void onPageFinished(WebView view, String url) {
+                            loaderView.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                            Log.e("TAG", "onReceivedError: " + error.toString() );
+                        }
+
+                    });
+
+                    String newhtml_code = Base64.encodeToString(knowledgeBaseArticle.content.getBytes(), Base64.NO_PADDING);
+
+                    webView.getSettings().setJavaScriptEnabled(true);
+                    webView.loadData(newhtml_code, "text/html", "base64");
+
                 }
             }
         }
@@ -140,5 +176,14 @@ public class FaqDetailActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         config.setActivityConfig(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
